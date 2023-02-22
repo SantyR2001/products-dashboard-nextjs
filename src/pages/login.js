6bf02e30db5ff6fc1dea7d22b2998e4656c11e50
Santiago Raab <santiagoraab@gmail.com>
2023-02-22 21:23:23 +0000

    add login page

---
+++ src/pages/login.js
@@ -0,0 +1,12 @@
+import LoginPage from '@components/LoginPage';
+import React from 'react';
+
+function login(props) {
+  return (
+    <>
+      <LoginPage />
+    </>
+  );
+}
+
+export default login;
